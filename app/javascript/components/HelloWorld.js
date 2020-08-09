0b4799f1af17ea5cb9cb013124dcecee9978c9e5
OswaldoPineda <carlos-1502010@hotmail.com>
2020-08-09 02:54:37 -0500

    Create HelloWord component

---
+++ app/javascript/components/HelloWorld.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class HelloWorld extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        hi from react!!
+      </React.Fragment>
+    );
+  }
+}
+
+export default HelloWorld
