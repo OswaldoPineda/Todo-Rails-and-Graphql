eb6a5b82d9c45968d9b0ff0dcd0b233aeda30410
OswaldoPineda <carlos-1502010@hotmail.com>
2020-09-25 23:26:57 -0500

    Create Task Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_15_222702) do
+ActiveRecord::Schema.define(version: 2020_09_26_043814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 2020_09_15_222702) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_lists_on_user_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "list_id", null: false
+    t.index ["list_id"], name: "index_tasks_on_list_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -50,4 +59,5 @@ ActiveRecord::Schema.define(version: 2020_09_15_222702) do
   end
 
   add_foreign_key "lists", "users"
+  add_foreign_key "tasks", "lists"
 end
